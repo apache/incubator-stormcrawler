a3302b082e51856b9dbb4f1648c9aaf5ac2fdf34
Julien Nioche <julien@digitalpebble.com>
2014-08-29 16:02:16 +0100

    Added ConfigurableTopology

---
+++ src/main/java/com/digitalpebble/storm/crawler/ConfigurableTopology.java
@@ -0,0 +1,93 @@
+package com.digitalpebble.storm.crawler;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.topology.TopologyBuilder;
+
+public abstract class ConfigurableTopology {
+
+	public static void start(ConfigurableTopology topology, String args[]) {
+		String[] remainingArgs = topology.parse(args);
+		topology.run(remainingArgs);
+	}
+
+	protected Config conf = new Config();
+
+	protected boolean isLocal = false;
+
+	protected Config getConf() {
+		return conf;
+	}
+
+	protected abstract int run(String args[]);
+
+	protected int submit(String name, Config conf, TopologyBuilder builder) {
+		if (isLocal) {
+			LocalCluster cluster = new LocalCluster();
+			cluster.submitTopology(name, conf, builder.createTopology());
+		}
+
+		else {
+			try {
+				StormSubmitter.submitTopology(name, conf,
+						builder.createTopology());
+			} catch (Exception e) {
+				e.printStackTrace();
+				return -1;
+			}
+		}
+		return 0;
+	}
+
+	private String[] parse(String args[]) {
+
+		List<String> newArgs = new ArrayList<String>();
+		Collections.addAll(newArgs, args);
+
+		Iterator<String> iter = newArgs.iterator();
+		while (iter.hasNext()) {
+			String param = iter.next();
+			if (param.equals("-conf")) {
+				if (!iter.hasNext()) {
+					System.err.println("Missing conf file");
+					System.exit(-1);
+				}
+				iter.remove();
+				String resource = iter.next();
+				Yaml yaml = new Yaml();
+				Map ret = null;
+				try {
+					ret = (Map) yaml.load(new InputStreamReader(
+							new FileInputStream(resource)));
+				} catch (FileNotFoundException e) {
+					System.err
+							.println("Conf file does not exist : " + resource);
+					System.exit(-1);
+				}
+				if (ret == null)
+					ret = new HashMap();
+				conf.putAll(ret);
+				iter.remove();
+			} else if (param.equals("-local")) {
+				isLocal = true;
+				iter.remove();
+			}
+		}
+
+		return (String[]) newArgs.toArray(new String[newArgs.size()]);
+	}
+
+}

3ec6537945acab717375f959c18dc0d9e201c5e6
Julien Nioche <julien@digitalpebble.com>
2014-09-04 09:47:01 +0100

    CrawlTopology now extends ConfigurableTopology + added crawler-conf.yaml

--- src/main/java/com/digitalpebble/storm/crawler/CrawlTopology.java
+++ src/main/java/com/digitalpebble/storm/crawler/CrawlTopology.java
@@ -1,8 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.digitalpebble.storm.crawler;
 
-import backtype.storm.Config;
-import backtype.storm.LocalCluster;
-import backtype.storm.StormSubmitter;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
 
@@ -15,10 +29,14 @@ import com.digitalpebble.storm.crawler.spout.RandomURLSpout;
 /**
  * Dummy topology to play with the spouts and bolts
  */
-public class CrawlTopology {
+public class CrawlTopology extends ConfigurableTopology {
 
     public static void main(String[] args) throws Exception {
+        ConfigurableTopology.start(new CrawlTopology(), args);
+    }
 
+    @Override
+    protected int run(String[] args) {
         TopologyBuilder builder = new TopologyBuilder();
 
         builder.setSpout("spout", new RandomURLSpout());
@@ -32,24 +50,9 @@ public class CrawlTopology {
 
         builder.setBolt("index", new IndexerBolt()).shuffleGrouping("parse");
 
-        Config conf = new Config();
-        conf.setDebug(true);
         conf.registerMetricsConsumer(DebugMetricConsumer.class);
 
-        if (args != null && args.length > 0) {
-            conf.setNumWorkers(3);
-
-            StormSubmitter.submitTopology(args[0], conf,
-                    builder.createTopology());
-        } else {
-            conf.setMaxTaskParallelism(3);
-
-            LocalCluster cluster = new LocalCluster();
-            cluster.submitTopology("crawl", conf, builder.createTopology());
-
-            Thread.sleep(10000);
-
-            cluster.shutdown();
-        }
+        return submit("crawl", conf, builder);
     }
+
 }

7d78cb73636f2093cf72a20fac7d7f2c33a9152f
Julien Nioche <julien@digitalpebble.com>
2015-03-02 16:43:24 +0000

    Applied formatting to ESSpout + added default value for throttling to config

--- external/src/main/java/com/digitalpebble/storm/crawler/elasticsearch/persistence/ElasticSearchSpout.java
+++ external/src/main/java/com/digitalpebble/storm/crawler/elasticsearch/persistence/ElasticSearchSpout.java
@@ -49,186 +49,186 @@ import com.digitalpebble.storm.crawler.util.URLPartitioner;
  * **/
 public class ElasticSearchSpout extends BaseRichSpout {
 
-	private static final Logger LOG = LoggerFactory
-			.getLogger(ElasticSearchSpout.class);
+    private static final Logger LOG = LoggerFactory
+            .getLogger(ElasticSearchSpout.class);
 
-	private static final String ESBoltType = "status";
+    private static final String ESBoltType = "status";
 
-	private static final String ESStatusIndexNameParamName = "es.status.index.name";
-	private static final String ESStatusDocTypeParamName = "es.status.doc.type";
-	private static final String ESStatusMaxInflightParamName = "es.status.max.inflight.urls.per.bucket";
+    private static final String ESStatusIndexNameParamName = "es.status.index.name";
+    private static final String ESStatusDocTypeParamName = "es.status.doc.type";
+    private static final String ESStatusMaxInflightParamName = "es.status.max.inflight.urls.per.bucket";
 
-	private String indexName;
-	private String docType;
+    private String indexName;
+    private String docType;
 
-	private SpoutOutputCollector _collector;
+    private SpoutOutputCollector _collector;
 
-	private Client client;
+    private Client client;
 
-	private final int bufferSize = 100;
+    private final int bufferSize = 100;
 
-	private Queue<Values> buffer = new LinkedList<Values>();
+    private Queue<Values> buffer = new LinkedList<Values>();
 
-	private int lastStartOffset = 0;
+    private int lastStartOffset = 0;
 
-	private URLPartitioner partitioner;
+    private URLPartitioner partitioner;
 
-	private int maxInFlightURLsPerBucket = -1;
+    private int maxInFlightURLsPerBucket = -1;
 
-	/** Keeps a count of the URLs being processed per host/domain/IP **/
-	private Map<String, Integer> inFlightTracker = new HashMap<String, Integer>();
+    /** Keeps a count of the URLs being processed per host/domain/IP **/
+    private Map<String, Integer> inFlightTracker = new HashMap<String, Integer>();
 
-	// URL / politeness bucket (hostname / domain etc...)
-	private Map<String, String> beingProcessed = new HashMap<String, String>();
+    // URL / politeness bucket (hostname / domain etc...)
+    private Map<String, String> beingProcessed = new HashMap<String, String>();
 
-	@Override
-	public void open(Map stormConf, TopologyContext context,
-			SpoutOutputCollector collector) {
-		indexName = ConfUtils.getString(stormConf, ESStatusIndexNameParamName,
-				"status");
-		docType = ConfUtils.getString(stormConf, ESStatusDocTypeParamName,
-				"status");
-		maxInFlightURLsPerBucket = ConfUtils.getInt(stormConf,
-				ESStatusMaxInflightParamName, 1);
-		try {
-			client = ElasticSearchConnection.getClient(stormConf, ESBoltType);
-		} catch (Exception e1) {
-			LOG.error("Can't connect to ElasticSearch", e1);
-			throw new RuntimeException(e1);
-		}
+    @Override
+    public void open(Map stormConf, TopologyContext context,
+            SpoutOutputCollector collector) {
+        indexName = ConfUtils.getString(stormConf, ESStatusIndexNameParamName,
+                "status");
+        docType = ConfUtils.getString(stormConf, ESStatusDocTypeParamName,
+                "status");
+        maxInFlightURLsPerBucket = ConfUtils.getInt(stormConf,
+                ESStatusMaxInflightParamName, 1);
+        try {
+            client = ElasticSearchConnection.getClient(stormConf, ESBoltType);
+        } catch (Exception e1) {
+            LOG.error("Can't connect to ElasticSearch", e1);
+            throw new RuntimeException(e1);
+        }
 
-		partitioner = new URLPartitioner();
-		partitioner.configure(stormConf);
+        partitioner = new URLPartitioner();
+        partitioner.configure(stormConf);
 
-		_collector = collector;
-	}
+        _collector = collector;
+    }
 
-	@Override
-	public void close() {
-		if (client != null)
-			client.close();
-	}
+    @Override
+    public void close() {
+        if (client != null)
+            client.close();
+    }
 
-	@Override
-	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-		declarer.declare(new Fields("url", "metadata"));
-	}
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("url", "metadata"));
+    }
 
-	@Override
-	public void nextTuple() {
-		// have anything in the buffer?
-		if (!buffer.isEmpty()) {
-			Values fields = buffer.remove();
-			String url = fields.get(0).toString();
-			Metadata metadata = (Metadata) fields.get(1);
+    @Override
+    public void nextTuple() {
+        // have anything in the buffer?
+        if (!buffer.isEmpty()) {
+            Values fields = buffer.remove();
+            String url = fields.get(0).toString();
+            Metadata metadata = (Metadata) fields.get(1);
 
-			String partitionKey = partitioner.getPartition(url, metadata);
+            String partitionKey = partitioner.getPartition(url, metadata);
 
-			// check whether we already have too tuples in flight for this
-			// partition key
+            // check whether we already have too tuples in flight for this
+            // partition key
 
-			if (maxInFlightURLsPerBucket != -1) {
-				Integer inflightforthiskey = inFlightTracker.get(partitionKey);
-				if (inflightforthiskey == null)
-					inflightforthiskey = new Integer(0);
-				if (inflightforthiskey.intValue() >= maxInFlightURLsPerBucket) {
-					// do it later! left it out of the queue for now
-					return;
-				}
-				int currentCount = inflightforthiskey.intValue();
-				inFlightTracker.put(partitionKey, ++currentCount);
-			}
+            if (maxInFlightURLsPerBucket != -1) {
+                Integer inflightforthiskey = inFlightTracker.get(partitionKey);
+                if (inflightforthiskey == null)
+                    inflightforthiskey = new Integer(0);
+                if (inflightforthiskey.intValue() >= maxInFlightURLsPerBucket) {
+                    // do it later! left it out of the queue for now
+                    return;
+                }
+                int currentCount = inflightforthiskey.intValue();
+                inFlightTracker.put(partitionKey, ++currentCount);
+            }
 
-			beingProcessed.put(url, partitionKey);
+            beingProcessed.put(url, partitionKey);
 
-			this._collector.emit(fields, url);
-			return;
-		}
-		// re-populate the buffer
-		populateBuffer();
-	}
+            this._collector.emit(fields, url);
+            return;
+        }
+        // re-populate the buffer
+        populateBuffer();
+    }
 
-	/** run a query on ES to populate the internal buffer **/
-	private void populateBuffer() {
-		// TODO cap the number of results per shard
-		// assuming that the sharding of status URLs is done
-		// based on the hostname domain or anything else
-		// which is useful for politeness
+    /** run a query on ES to populate the internal buffer **/
+    private void populateBuffer() {
+        // TODO cap the number of results per shard
+        // assuming that the sharding of status URLs is done
+        // based on the hostname domain or anything else
+        // which is useful for politeness
 
-		// TODO cap the results per host or domain
+        // TODO cap the results per host or domain
 
-		Date now = new Date();
+        Date now = new Date();
 
-		// TODO use scrolls instead?
-		// @see
-		// http://www.elasticsearch.org/guide/en/elasticsearch/client/java-api/current/search.html#scrolling
-		SearchResponse response = client
-				.prepareSearch(indexName)
-				.setTypes(docType)
-				.setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-				.setQuery(QueryBuilders.rangeQuery("nextFetchDate").lte(now))
-				// .setPostFilter(
-				// FilterBuilders.rangeFilter("age").from(12).to(18))
-				.setFrom(lastStartOffset).setSize(this.bufferSize)
-				.setExplain(false).execute().actionGet();
+        // TODO use scrolls instead?
+        // @see
+        // http://www.elasticsearch.org/guide/en/elasticsearch/client/java-api/current/search.html#scrolling
+        SearchResponse response = client
+                .prepareSearch(indexName)
+                .setTypes(docType)
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(QueryBuilders.rangeQuery("nextFetchDate").lte(now))
+                // .setPostFilter(
+                // FilterBuilders.rangeFilter("age").from(12).to(18))
+                .setFrom(lastStartOffset).setSize(this.bufferSize)
+                .setExplain(false).execute().actionGet();
 
-		SearchHits hits = response.getHits();
-		lastStartOffset = hits.getHits().length;
+        SearchHits hits = response.getHits();
+        lastStartOffset = hits.getHits().length;
 
-		// filter results so that we don't include URLs we are already
-		// being processed or skip those for which we already have enough
-		//
-		for (int i = 0; i < hits.getHits().length; i++) {
-			Map<String, Object> keyValues = hits.getHits()[i].sourceAsMap();
-			String url = (String) keyValues.get("url");
+        // filter results so that we don't include URLs we are already
+        // being processed or skip those for which we already have enough
+        //
+        for (int i = 0; i < hits.getHits().length; i++) {
+            Map<String, Object> keyValues = hits.getHits()[i].sourceAsMap();
+            String url = (String) keyValues.get("url");
 
-			// is already being processed - skip it!
-			if (beingProcessed.containsKey(url))
-				continue;
+            // is already being processed - skip it!
+            if (beingProcessed.containsKey(url))
+                continue;
 
-			String mdAsString = (String) keyValues.get("metadata");
-			Metadata metadata = new Metadata();
-			if (mdAsString != null) {
-				// parse the string and generate the MD accordingly
-				// url.path: http://www.lemonde.fr/
-				// depth: 1
-				String[] kvs = mdAsString.split("\n");
-				for (String pair : kvs) {
-					String[] kv = pair.split(": ");
-					if (kv.length != 2) {
-						LOG.info("Invalid key value pair {}", pair);
-						continue;
-					}
-					metadata.addValue(kv[0], kv[1]);
-				}
-			}
-			buffer.add(new Values(url, metadata));
-		}
-	}
+            String mdAsString = (String) keyValues.get("metadata");
+            Metadata metadata = new Metadata();
+            if (mdAsString != null) {
+                // parse the string and generate the MD accordingly
+                // url.path: http://www.lemonde.fr/
+                // depth: 1
+                String[] kvs = mdAsString.split("\n");
+                for (String pair : kvs) {
+                    String[] kv = pair.split(": ");
+                    if (kv.length != 2) {
+                        LOG.info("Invalid key value pair {}", pair);
+                        continue;
+                    }
+                    metadata.addValue(kv[0], kv[1]);
+                }
+            }
+            buffer.add(new Values(url, metadata));
+        }
+    }
 
-	@Override
-	public void ack(Object msgId) {
-		super.ack(msgId);
-		String partitionKey = beingProcessed.remove(msgId);
-		decrementPartitionKey(partitionKey);
-	}
+    @Override
+    public void ack(Object msgId) {
+        super.ack(msgId);
+        String partitionKey = beingProcessed.remove(msgId);
+        decrementPartitionKey(partitionKey);
+    }
 
-	@Override
-	public void fail(Object msgId) {
-		super.fail(msgId);
-		String partitionKey = beingProcessed.remove(msgId);
-		decrementPartitionKey(partitionKey);
-	}
+    @Override
+    public void fail(Object msgId) {
+        super.fail(msgId);
+        String partitionKey = beingProcessed.remove(msgId);
+        decrementPartitionKey(partitionKey);
+    }
 
-	private void decrementPartitionKey(String partitionKey) {
-		if (partitionKey == null)
-			return;
-		Integer currentValue = this.inFlightTracker.get(partitionKey);
-		if (currentValue == null)
-			return;
-		int currentVal = currentValue.intValue();
-		currentVal--;
-		this.inFlightTracker.put(partitionKey, currentVal);
-	}
+    private void decrementPartitionKey(String partitionKey) {
+        if (partitionKey == null)
+            return;
+        Integer currentValue = this.inFlightTracker.get(partitionKey);
+        if (currentValue == null)
+            return;
+        int currentVal = currentValue.intValue();
+        currentVal--;
+        this.inFlightTracker.put(partitionKey, currentVal);
+    }
 
 }

db52fefc8de20b8eebcf3fea65af713245cd246d
Julien Nioche <julien@digitalpebble.com>
2015-03-25 16:02:51 +0000

    [ElasticSearch] Indexer to extend AbstractIndexerBolt #112

--- external/src/main/java/com/digitalpebble/storm/crawler/elasticsearch/bolt/IndexerBolt.java
+++ external/src/main/java/com/digitalpebble/storm/crawler/elasticsearch/bolt/IndexerBolt.java
@@ -20,10 +20,9 @@ package com.digitalpebble.storm.crawler.elasticsearch.bolt;
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 import java.io.IOException;
-import java.util.Date;
+import java.util.Iterator;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
 import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.slf4j.Logger;
@@ -33,11 +32,11 @@ import backtype.storm.metric.api.MultiCountMetric;
 import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.base.BaseRichBolt;
-import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 
+import com.digitalpebble.storm.crawler.Metadata;
 import com.digitalpebble.storm.crawler.elasticsearch.ElasticSearchConnection;
+import com.digitalpebble.storm.crawler.indexing.AbstractIndexerBolt;
 import com.digitalpebble.storm.crawler.util.ConfUtils;
 
 /**
@@ -45,7 +44,7 @@ import com.digitalpebble.storm.crawler.util.ConfUtils;
  * Map &lt;String,Object&gt; from a named field.
  */
 @SuppressWarnings("serial")
-public class IndexerBolt extends BaseRichBolt {
+public class IndexerBolt extends AbstractIndexerBolt {
 
     private static final Logger LOG = LoggerFactory
             .getLogger(IndexerBolt.class);
@@ -55,36 +54,26 @@ public class IndexerBolt extends BaseRichBolt {
     private static final String ESIndexNameParamName = "es.indexer.index.name";
     private static final String ESDocTypeParamName = "es.indexer.doc.type";
 
-    private static final String ESInputFieldParamName = "es.indexer.input.fieldname";
-    private static final String ESGenerateTimeStampParamName = "es.indexer.generate.timestamp";
-
     private OutputCollector _collector;
 
     private String indexName;
     private String docType;
 
-    private String inputField;
-    private boolean generateTimeStamp;
-
     private MultiCountMetric eventCounter;
 
     private ElasticSearchConnection connection;
 
-    @SuppressWarnings("resource")
+    @SuppressWarnings({ "unchecked", "rawtypes" })
     @Override
     public void prepare(Map conf, TopologyContext context,
             OutputCollector collector) {
+        super.prepare(conf, context, collector);
         _collector = collector;
 
         indexName = ConfUtils.getString(conf, IndexerBolt.ESIndexNameParamName,
                 "fetcher");
         docType = ConfUtils.getString(conf, IndexerBolt.ESDocTypeParamName,
                 "doc");
-
-        inputField = ConfUtils.getString(conf,
-                IndexerBolt.ESInputFieldParamName, null);
-        generateTimeStamp = ConfUtils.getBoolean(conf,
-                IndexerBolt.ESGenerateTimeStampParamName, false);
 
         try {
             connection = ElasticSearchConnection
@@ -107,41 +96,41 @@ public class IndexerBolt extends BaseRichBolt {
     @Override
     public void execute(Tuple tuple) {
 
+        String url = tuple.getStringByField("url");
+        Metadata metadata = (Metadata) tuple.getValueByField("metadata");
+        String text = tuple.getStringByField("text");
+
+        boolean keep = filterDocument(metadata);
+        if (!keep) {
+            eventCounter.scope("Filtered").incrBy(1);
+            _collector.ack(tuple);
+            return;
+        }
+
         try {
             XContentBuilder builder = jsonBuilder().startObject();
 
-            boolean hasTimeStamp = false;
-
-            // field where the map containing the fields is stored
-            if (StringUtils.isNotBlank(this.inputField)) {
-                Map<String, Object> f = (Map<String, Object>) tuple
-                        .getValueByField(this.inputField);
-
-                java.util.Iterator<String> iter = f.keySet().iterator();
-
-                while (iter.hasNext()) {
-                    String name = iter.next();
-                    Object value = f.get(name);
-                    if (name.equalsIgnoreCase("timestamp")) {
-                        hasTimeStamp = true;
-                    }
-                    builder.field(name, value);
-                }
-            }
-            // use all the fields present in the tuple
-            else {
-                Fields fs = tuple.getFields();
-                for (int i = 0; i < fs.size(); i++) {
-                    String name = fs.get(i);
-                    if (name.equalsIgnoreCase("timestamp")) {
-                        hasTimeStamp = true;
-                    } else
-                        builder.field(name, tuple.getValue(i));
-                }
+            // display text of the document?
+            if (fieldNameForText() != null) {
+                builder.field(fieldNameForText(), text);
             }
 
-            if (generateTimeStamp && !hasTimeStamp)
-                builder.field("timestamp", new Date());
+            // send URL as field?
+            if (fieldNameForURL() != null) {
+                builder.field(fieldNameForURL(), url);
+            }
+
+            // which metadata to display?
+            Map<String, String[]> keyVals = filterMetadata(metadata);
+
+            Iterator<String> iterator = keyVals.keySet().iterator();
+            while (iterator.hasNext()) {
+                String fieldName = iterator.next();
+                String[] values = keyVals.get(fieldName);
+                for (String value : values) {
+                    builder.field(fieldName, value);
+                }
+            }
 
             builder.endObject();
 

778b7ed4cd77274b4e8e6d6639654adb2f971faf
Julien Nioche <julien@digitalpebble.com>
2015-04-17 12:59:59 +0100

    Fetcher : can configure a custom number of threads per queue ID

--- core/src/main/java/com/digitalpebble/storm/crawler/bolt/FetcherBolt.java
+++ core/src/main/java/com/digitalpebble/storm/crawler/bolt/FetcherBolt.java
@@ -273,7 +273,7 @@ public class FetcherBolt extends BaseRichBolt {
 
         AtomicInteger inQueues = new AtomicInteger(0);
 
-        final int maxThreads;
+        final int defaultMaxThread;
         final long crawlDelay;
         final long minCrawlDelay;
 
@@ -287,7 +287,7 @@ public class FetcherBolt extends BaseRichBolt {
 
         public FetchItemQueues(Config conf) {
             this.conf = conf;
-            this.maxThreads = ConfUtils.getInt(conf,
+            this.defaultMaxThread = ConfUtils.getInt(conf,
                     "fetcher.threads.per.queue", 1);
             queueMode = ConfUtils.getString(conf, "fetcher.queue.mode",
                     QUEUE_MODE_HOST);
@@ -333,8 +333,11 @@ public class FetcherBolt extends BaseRichBolt {
         public synchronized FetchItemQueue getFetchItemQueue(String id) {
             FetchItemQueue fiq = queues.get(id);
             if (fiq == null) {
+                // custom maxThread value?
+                final int customThreadVal = ConfUtils.getInt(conf,
+                        "fetcher.maxThreads." + id, defaultMaxThread);
                 // initialize queue
-                fiq = new FetchItemQueue(conf, maxThreads, crawlDelay,
+                fiq = new FetchItemQueue(conf, customThreadVal, crawlDelay,
                         minCrawlDelay);
                 queues.put(id, fiq);
 

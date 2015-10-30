08ec247ce262d3447b354fa862f5891d0d9c94d9
Julien Nioche <julien@digitalpebble.com>
2015-10-30 13:45:25 +0000

    PerSecondReducer for Storm metrics + used in Fetcher bolts for bytes and docs per sec

---
+++ core/src/main/java/com/digitalpebble/storm/crawler/util/PerSecondReducer.java
@@ -0,0 +1,42 @@
+package com.digitalpebble.storm.crawler.util;
+
+import backtype.storm.metric.api.IReducer;
+
+/** Used to return an average value per second **/
+public class PerSecondReducer implements IReducer<TimeReducerState> {
+
+    @Override
+    public TimeReducerState init() {
+        return new TimeReducerState();
+    }
+
+    @Override
+    public TimeReducerState reduce(TimeReducerState accumulator, Object input) {
+        if (input instanceof Double) {
+            accumulator.sum += (Double) input;
+        } else if (input instanceof Long) {
+            accumulator.sum += ((Long) input).doubleValue();
+        } else if (input instanceof Integer) {
+            accumulator.sum += ((Integer) input).doubleValue();
+        } else {
+            throw new RuntimeException(
+                    "MeanReducer::reduce called with unsupported input type `"
+                            + input.getClass()
+                            + "`. Supported types are Double, Long, Integer.");
+        }
+        return accumulator;
+    }
+
+    @Override
+    public Object extractResult(TimeReducerState accumulator) {
+        // time spent
+        long sec = (System.currentTimeMillis() - accumulator.started) / 1000;
+        return new Double(accumulator.sum / (double) sec);
+    }
+
+}
+
+class TimeReducerState {
+    public long started = System.currentTimeMillis();
+    public double sum = 0.0;
+}

5c60af26a7f163c3201ff1003670f87b59f1d040
Owen <owen@cameraforensics.com>
2016-09-06 11:27:22 +0100

    Added metadata.lastProcessedDate

--- core/src/main/java/com/digitalpebble/stormcrawler/persistence/AbstractStatusUpdaterBolt.java
+++ core/src/main/java/com/digitalpebble/stormcrawler/persistence/AbstractStatusUpdaterBolt.java
@@ -16,7 +16,10 @@
  */
 package com.digitalpebble.stormcrawler.persistence;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -43,8 +46,9 @@ import com.google.common.cache.CacheBuilder;
 @SuppressWarnings("serial")
 public abstract class AbstractStatusUpdaterBolt extends BaseRichBolt {
 
-    private static final Logger LOG = LoggerFactory
-            .getLogger(AbstractStatusUpdaterBolt.class);
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractStatusUpdaterBolt.class);
+
+    private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 
     /**
      * Parameter name to indicate whether the internal cache should be used for
@@ -153,6 +157,10 @@ public abstract class AbstractStatusUpdaterBolt extends BaseRichBolt {
         if (!status.equals(Status.FETCH_ERROR)) {
             metadata.remove(Constants.fetchErrorCountParamName);
         }
+
+        // store last processed date
+        final Date lastProcessed = new Date();
+        metadata.setValue("lastProcessedDate", dateFormat.format(lastProcessed));
 
         // determine the value of the next fetch based on the status
         Date nextFetch = scheduler.schedule(status, metadata);

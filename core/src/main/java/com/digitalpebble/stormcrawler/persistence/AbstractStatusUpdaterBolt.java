fd8e793946f71bc0b95c5ecf97501c2d9878bcf9
Owen <owen@cameraforensics.com>
2016-09-21 15:01:52 +0100

    Formatted code.

--- core/src/main/java/com/digitalpebble/stormcrawler/persistence/AbstractStatusUpdaterBolt.java
+++ core/src/main/java/com/digitalpebble/stormcrawler/persistence/AbstractStatusUpdaterBolt.java
@@ -162,7 +162,8 @@ public abstract class AbstractStatusUpdaterBolt extends BaseRichBolt {
         // store last processed date
         if (!status.equals(Status.DISCOVERED)) {
             final Date lastProcessed = new Date();
-            metadata.setValue("lastProcessedDate", dateFormat.format(lastProcessed));
+            metadata.setValue("lastProcessedDate",
+                    dateFormat.format(lastProcessed));
         }
 
         // determine the value of the next fetch based on the status

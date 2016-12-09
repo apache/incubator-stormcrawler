b8d4c43ae2c3645f198f34c200aa785e47219cf0
Julien Nioche <julien@digitalpebble.com>
2016-12-09 09:45:15 +0000

    ES 5.1.1

--- external/elasticsearch/src/main/java/com/digitalpebble/stormcrawler/elasticsearch/persistence/StatusUpdaterBolt.java
+++ external/elasticsearch/src/main/java/com/digitalpebble/stormcrawler/elasticsearch/persistence/StatusUpdaterBolt.java
@@ -156,7 +156,7 @@ public class StatusUpdaterBolt extends AbstractStatusUpdaterBolt {
                         executionId, throwable);
                 // WHOLE BULK FAILED
                 // mark all the docs as fail
-                Iterator<ActionRequest<?>> itreq = request.requests()
+                Iterator<ActionRequest> itreq = request.requests()
                         .iterator();
                 while (itreq.hasNext()) {
                     IndexRequest bir = (IndexRequest) itreq.next();

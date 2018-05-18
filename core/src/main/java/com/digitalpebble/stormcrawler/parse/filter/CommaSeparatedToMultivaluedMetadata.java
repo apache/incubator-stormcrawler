8006c9245d8d3069275c55203641f06b12677064
Julien Nioche <julien@digitalpebble.com>
2018-05-18 12:34:47 +0100

    Add ParseFilter to convert single valued Metadata to multi-valued ones, fixes #571

---
+++ core/src/main/java/com/digitalpebble/stormcrawler/parse/filter/CommaSeparatedToMultivaluedMetadata.java
@@ -0,0 +1,73 @@
+/**
+ * Licensed to DigitalPebble Ltd under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * DigitalPebble licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.digitalpebble.stormcrawler.parse.filter;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.w3c.dom.DocumentFragment;
+
+import com.digitalpebble.stormcrawler.Metadata;
+import com.digitalpebble.stormcrawler.parse.ParseFilter;
+import com.digitalpebble.stormcrawler.parse.ParseResult;
+import com.fasterxml.jackson.databind.JsonNode;
+
+/**
+ * Rewrites single metadata containing comma separated values into multiple
+ * values for the same key, useful for instance for keyword tags.
+ **/
+
+public class CommaSeparatedToMultivaluedMetadata extends ParseFilter {
+
+    private final Set<String> keys = new HashSet<>();
+
+    @Override
+    public void configure(Map stormConf, JsonNode filterParams) {
+        JsonNode node = filterParams.get("keys");
+        if (node == null) {
+            return;
+        }
+        if (node.isArray()) {
+            Iterator<JsonNode> iter = node.iterator();
+            while (iter.hasNext()) {
+                keys.add(iter.next().asText());
+            }
+        } else {
+            keys.add(node.asText());
+        }
+    }
+
+    @Override
+    public void filter(String url, byte[] content, DocumentFragment doc,
+            ParseResult parse) {
+        Metadata m = parse.get(url).getMetadata();
+        for (String key : keys) {
+            String val = m.getFirstValue(key);
+            if (val == null)
+                continue;
+            m.remove(key);
+            String[] tokens = val.split(" *, *");
+            for (String t : tokens) {
+                m.addValue(key, t);
+            }
+        }
+    }
+
+}
---
+++ core/src/test/java/com/digitalpebble/stormcrawler/parse/filter/CSVMetadataFilterTest.java
@@ -0,0 +1,55 @@
+/**
+ * Licensed to DigitalPebble Ltd under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * DigitalPebble licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.digitalpebble.stormcrawler.parse.filter;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.digitalpebble.stormcrawler.Metadata;
+import com.digitalpebble.stormcrawler.bolt.JSoupParserBolt;
+import com.digitalpebble.stormcrawler.parse.ParsingTester;
+
+public class CSVMetadataFilterTest extends ParsingTester {
+
+    @Before
+    public void setupParserBolt() {
+        bolt = new JSoupParserBolt();
+        setupParserBolt(bolt);
+    }
+
+    @Test
+    public void testMultivalued() throws IOException {
+
+        prepareParserBolt("test.parsefilters.json");
+
+        parse("http://www.digitalpebble.com", "digitalpebble.com.html");
+
+        Assert.assertEquals(1, output.getEmitted().size());
+        List<Object> parsedTuple = output.getEmitted().get(0);
+        Metadata metadata = (Metadata) parsedTuple.get(2);
+        Assert.assertNotNull(metadata);
+        String[] kws = metadata.getValues("keywords");
+        Assert.assertNotNull(kws);
+        Assert.assertEquals(12, kws.length);
+
+    }
+}

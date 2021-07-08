dcbac315bd3f37578946ef69245dbef70dc46ac4
Sam Ulrich <40002776+sam-ulrich1@users.noreply.github.com>
2021-07-08 08:31:24 -0500

    Multi proxy support (#890)
    * Added multi-proxy support
    
    Modified ProxyManager abstract to enable more advanced proxy implementations. SingleProxyManager can be used to preserve the original functionality of SC's proxy system. MultiProxyManager can be used to load a list of proxies from a file and balance usage across them. Currently the balancing algorithms are Round Robin, Random Selection, and Least Used. 
    
    New proxy management algorithms and use-cases can be implemented by extending the abstract class ProxyManager and changing the config value `http.proxy.manager` to point to the new implementation.
    
    The SCProxy class is setup to handle Geo information as well. Future implementations could take advantage of this and handle proxies for Geo blocked content.
    
    Signed-off-by: Sam Ulrich <samson98ulrich@gmail.com>
    
    * ProxyManager impl to into abstract & converted ProxyManager to interace
    
    Signed-off-by: Sam Ulrich <samson98ulrich@gmail.com>
    
    * Modifications to resolve SCs requested changes
    
    - Changed proxy manager interface to receive the full Storm config and load all config based parameters directly from the config
    - Changed SCProxy variables to private with getters
    - Changed SCProxy regex variable to private final
    - Removed `ready` function from proxy managers
    - Added debug logging for time associated with building the client/request config in the protocol implementations
    - Moved ProxyRotation to a local enum for MultiProxyManager
    
    Signed-off-by: Sam Ulrich <samson98ulrich@gmail.com>
    
    * Removed accidental import in AbstractHttpProtocol
    
    Signed-off-by: Sam Ulrich <samson98ulrich@gmail.com>
    
    * Delete .attach_pid2715970
    
    * Changed ProxyManager exceptions to RuntimeException
    
    Signed-off-by: Sam Ulrich <samson98ulrich@gmail.com>
    
    * Removed RuntineException for ProxyManager signature
    
    Signed-off-by: Sam Ulrich <samson98ulrich@gmail.com>
    
    Co-authored-by: Julien Nioche <julien@digitalpebble.com>

---
+++ core/src/test/java/com/digitalpebble/stormcrawler/proxy/SingleProxyManagerTest.java
@@ -0,0 +1,50 @@
+/**
+ * Licensed to DigitalPebble Ltd under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * DigitalPebble licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.digitalpebble.stormcrawler.proxy;
+
+import com.digitalpebble.stormcrawler.util.ConfUtils;
+import org.apache.storm.Config;
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.io.FileNotFoundException;
+
+public class SingleProxyManagerTest {
+    @Test
+    public void testSimpleProxyManager() throws RuntimeException {
+        Config config = new Config();
+        config.put("http.proxy.host", "example.com");
+        config.put("http.proxy.type", "HTTP");
+        config.put("http.proxy.port", 8080);
+        config.put("http.proxy.user", "user1");
+        config.put("http.proxy.pass", "pass1");
+
+        SingleProxyManager pm = new SingleProxyManager();
+        pm.configure(config);
+
+        SCProxy proxy = pm.getProxy();
+
+        Assert.assertEquals(proxy.getProtocol(), "http");
+        Assert.assertEquals(proxy.getAddress(), "example.com");
+        Assert.assertEquals(proxy.getPort(), "8080");
+        Assert.assertEquals(proxy.getUsername(), "user1");
+        Assert.assertEquals(proxy.getPassword(), "pass1");
+
+        Assert.assertEquals(proxy.toString(), "http://user1:pass1@example.com:8080");
+    }
+}

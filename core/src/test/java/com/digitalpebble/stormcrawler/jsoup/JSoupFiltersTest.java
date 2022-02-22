76cf0faca9859979ad338f34b36eb3a924c9fd26
Julien Nioche <julien@digitalpebble.com>
2022-02-22 12:13:45 +0000

    Rewrote LinkParseFUlter + added XPathFilter + tests for JSOUPFilters (#953)
    * Rewrote LinkParseFUlter + added XPathFilter + tests for JSOUPFilters
    
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>
    
    * use jsoup filters in archetypes
    
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>
    
    * Fix package name XpathFilter
    
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>
    
    * Make inner class LabelledExpression static
    
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>

---
+++ core/src/test/java/com/digitalpebble/stormcrawler/jsoup/JSoupFiltersTest.java
@@ -0,0 +1,106 @@
+/**
+ * Licensed to DigitalPebble Ltd under one or more contributor license agreements. See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership.
+ * DigitalPebble licenses this file to You under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.digitalpebble.stormcrawler.jsoup;
+
+import com.digitalpebble.stormcrawler.Metadata;
+import com.digitalpebble.stormcrawler.TestUtil;
+import com.digitalpebble.stormcrawler.bolt.JSoupParserBolt;
+import com.digitalpebble.stormcrawler.parse.ParsingTester;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import org.apache.storm.task.OutputCollector;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/** Test the JSoup filters * */
+public class JSoupFiltersTest extends ParsingTester {
+
+    @Before
+    public void setupParserBolt() {
+        bolt = new JSoupParserBolt();
+        setupParserBolt(bolt);
+    }
+
+    protected void prepareParserBolt(String configFile, Map parserConfig) {
+        parserConfig.put("jsoup.filters.config.file", configFile);
+        bolt.prepare(
+                parserConfig, TestUtil.getMockedTopologyContext(), new OutputCollector(output));
+    }
+
+    @Test
+    public void testBasicExtraction() throws IOException {
+
+        prepareParserBolt("test.jsoupfilters.json");
+
+        parse("http://www.digitalpebble.com", "digitalpebble.com.html");
+
+        Assert.assertEquals(1, output.getEmitted().size());
+        List<Object> parsedTuple = output.getEmitted().get(0);
+        Metadata metadata = (Metadata) parsedTuple.get(2);
+        Assert.assertNotNull(metadata);
+        String concept = metadata.getFirstValue("concept");
+        Assert.assertNotNull(concept);
+
+        concept = metadata.getFirstValue("concept2");
+        Assert.assertNotNull(concept);
+    }
+
+    @Test
+    // https://github.com/DigitalPebble/storm-crawler/issues/219
+    public void testScriptExtraction() throws IOException {
+
+        prepareParserBolt("test.jsoupfilters.json");
+
+        parse("http://www.digitalpebble.com", "digitalpebble.com.html");
+
+        Assert.assertEquals(1, output.getEmitted().size());
+        List<Object> parsedTuple = output.getEmitted().get(0);
+        Metadata metadata = (Metadata) parsedTuple.get(2);
+        Assert.assertNotNull(metadata);
+        String[] scripts = metadata.getValues("js");
+        Assert.assertNotNull(scripts);
+        // should be 2 of them
+        Assert.assertEquals(2, scripts.length);
+        Assert.assertEquals("", scripts[0].trim());
+        Assert.assertTrue(scripts[1].contains("urchinTracker();"));
+    }
+
+    @Test
+    public void testLDJsonExtraction() throws IOException {
+
+        prepareParserBolt("test.jsoupfilters.json");
+
+        parse("http://www.digitalpebble.com", "digitalpebble.com.html");
+
+        Assert.assertEquals(1, output.getEmitted().size());
+        List<Object> parsedTuple = output.getEmitted().get(0);
+        Metadata metadata = (Metadata) parsedTuple.get(2);
+        Assert.assertNotNull(metadata);
+        String[] scripts = metadata.getValues("streetAddress");
+        Assert.assertNotNull(scripts);
+    }
+
+    @Test
+    public void testExtraLink() throws IOException {
+
+        prepareParserBolt("test.jsoupfilters.json");
+
+        parse("http://www.digitalpebble.com", "digitalpebble.com.html");
+
+        Assert.assertEquals(16, output.getEmitted("status").size());
+    }
+}

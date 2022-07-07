7a622e5ab4276b4d0bd389bed4500badb900b257
Felix Engl <felix.engl@hotmail.com>
2022-07-07 12:50:18 +0200

    Add unit test basics for URLFrontier. (#984)
    * Add unit test for ACKing with URLFrontier

---
+++ external/urlfrontier/src/test/java/com/digitalpebble/stormcrawler/urlfrontier/URLFrontierContainerConfig.java
@@ -0,0 +1,31 @@
+/**
+ * Licensed to DigitalPebble Ltd under one or more contributor license agreements. See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership.
+ * DigitalPebble licenses this file to You under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.digitalpebble.stormcrawler.urlfrontier;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Range;
+
+public interface URLFrontierContainerConfig {
+    void setRocksPath(@NotNull String path);
+
+    @Nullable
+    String getRocksPath();
+
+    void setPrometheusPort(@Range(from = 0, to = 65535) int port);
+
+    @Range(from = 0, to = 65535)
+    int getPrometheusPort();
+}

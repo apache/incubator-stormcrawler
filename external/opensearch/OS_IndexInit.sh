a81e8a72c89c460cf3958accfeb81260efdbcbb1
Julien Nioche <julien@digitalpebble.com>
2022-12-09 16:26:17 +0000

    more compact IndexInitScript
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>

--- external/opensearch/OS_IndexInit.sh
+++ external/opensearch/OS_IndexInit.sh
@@ -1,29 +1,21 @@
 OSHOST=${1:-"http://localhost:9200"}
 OSCREDENTIALS=${2:-"-u opensearch:passwordhere"}
 
-curl $OSCREDENTIALS -s -XDELETE "$OSHOST/status/" >  /dev/null
+for name in status content
+do
+  curl $OSCREDENTIALS -s -XDELETE "$OSHOST/$name/" >  /dev/null
+  echo "Deleted '$name' index, now recreating it..."
+  curl $OSCREDENTIALS -s -XPUT $OSHOST/$name -H 'Content-Type: application/json' --upload-file src/main/resources/$name.mapping
+  echo ""
+done
 
-echo "Deleted status index"
-
-echo "Re-creating mapping for status index"
-
-curl $OSCREDENTIALS -s -XPUT $OSHOST/status -H 'Content-Type: application/json' --upload-file src/main/resources/status.mapping
-
-curl $OSCREDENTIALS -s -XDELETE "$OSHOST/content*/" >  /dev/null
-
-echo ""
-echo "Deleted content index"
-
-echo "Re-creating mapping for content index "
-
-curl $OSCREDENTIALS -s -XPUT $OSHOST/content -H 'Content-Type: application/json' --upload-file src/main/resources/content.mapping
+### metrics
 
 curl $OSCREDENTIALS -s -XDELETE "$OSHOST/metrics*/" >  /dev/null
 
-echo ""
-echo "Deleted metrics index"
-
-echo "Re-creating metrics template"
+echo "Deleted 'metrics' index, now recreating it..."
 
 # http://localhost:9200/metrics/_mapping/status?pretty
 curl $OSCREDENTIALS -s -XPOST $OSHOST/_template/metrics-template -H 'Content-Type: application/json' --upload-file src/main/resources/metrics.mapping
+
+echo ""

d8188746ec7125eb11b8755fef51c0b70c3462ee
Julien Nioche <julien@digitalpebble.com>
2023-06-29 08:29:11 +0100

    Test URL Filtering from the command line (#1081)
    * First attempt at URL checker - needs org/apache/storm/Config which is a bit of a problem
    
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>
    
    * Correct value for default name of parsefilter config file + can specify source (although doesn't seem to be working)
    
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>
    
    * source URL is taken from the 2nd argument if any
    
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>
    
    ---------
    
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>

--- core/src/main/java/com/digitalpebble/stormcrawler/filtering/URLFilters.java
+++ core/src/main/java/com/digitalpebble/stormcrawler/filtering/URLFilters.java
@@ -27,7 +27,14 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.commons.lang.StringUtils;
+import org.apache.storm.Config;
+import org.apache.storm.utils.Utils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.LoggerFactory;
@@ -49,7 +56,7 @@ public class URLFilters extends URLFilter implements JSONResource {
         filters = new URLFilters[0];
     }
 
-    private String configFile = "urlfilters.config.file";
+    private String configFile = "urlfilters.json";
 
     private Map<String, Object> stormConf;
 
@@ -127,5 +134,70 @@ public class URLFilters extends URLFilter implements JSONResource {
                 Configurable.createConfiguredInstance(
                         this.getClass(), URLFilter.class, stormConf, filtersConf);
         filters = list.toArray(new URLFilter[0]);
+    }
+
+    /** Utility to check the filtering of a URL * */
+    public static void main(String[] args) throws ParseException {
+
+        Config conf = new Config();
+
+        // loads the default configuration file
+        Map<String, Object> defaultSCConfig =
+                Utils.findAndReadConfigFile("crawler-default.yaml", false);
+        conf.putAll(ConfUtils.extractConfigElement(defaultSCConfig));
+
+        String configFile = "urlfilters.json";
+
+        Options options =
+                new Options()
+                        .addOption("f", true, "Filters configuration file. Default " + configFile);
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        if (cmd.hasOption("f")) {
+            configFile = cmd.getOptionValue("f");
+        }
+
+        if (cmd.getArgList().isEmpty()) {
+            System.err.println("Missing argument for input URL");
+            System.exit(-1);
+        }
+
+        // read URL to check
+        String inputURL = cmd.getArgList().get(0);
+
+        // if a URL has been specified in 2nd position
+        String sourceURL = inputURL;
+        if (cmd.getArgList().size() > 1) {
+            sourceURL = cmd.getArgList().get(1);
+        }
+
+        try {
+            URLFilters filters = new URLFilters(conf, configFile);
+            String normalizedURL = inputURL;
+            try {
+                for (URLFilter filter : filters.filters) {
+                    long start = System.currentTimeMillis();
+                    normalizedURL =
+                            filter.filter(new URL(sourceURL), new Metadata(), normalizedURL);
+                    long end = System.currentTimeMillis();
+                    System.out.println(
+                            "\t["
+                                    + filter.getClass().getName()
+                                    + "] "
+                                    + (end - start)
+                                    + "msec => "
+                                    + normalizedURL);
+                    if (normalizedURL == null) break;
+                }
+            } catch (Exception e) {
+                LOG.error("URL filtering threw exception", e);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        System.exit(0);
     }
 }

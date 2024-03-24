1b5c03843f85feea210a5330166028272733a81f
Julien Nioche <julien@digitalpebble.com>
2024-03-24 09:26:22 +0000

    OpenSearch - better handling of mappings  (#1155)
    * updated version of testcontainers + THIRD-PARTY.txt regenerated
    
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>
    
    * Mappings copied to archetype + fixed OS_IndexInit. Fixes #1153
    
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>
    
    ---------
    
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>

--- external/opensearch/archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ external/opensearch/archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,2 +1,5 @@
-def file2 = new File(request.getOutputDirectory(), request.getArtifactId() + "/dashboards/importDashboards.sh")
+def file1 = new File(request.getOutputDirectory(), request.getArtifactId() + "/dashboards/importDashboards.sh")
+file1.setExecutable(true, false)
+
+def file2 = new File(request.getOutputDirectory(), request.getArtifactId() + "/OS_IndexInit.sh")
 file2.setExecutable(true, false)

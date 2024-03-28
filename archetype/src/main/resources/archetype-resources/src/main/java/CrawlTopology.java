3b69841cdc41b14dbca864404c47b38d59ed2cf7
Richard Zowalla <rzo1@apache.org>
2024-03-28 16:59:25 +0100

    Fix #1164 - Change license headers
    Signed-off-by: Richard Zowalla <rzo1@apache.org>

--- archetype/src/main/resources/archetype-resources/src/main/java/CrawlTopology.java
+++ archetype/src/main/resources/archetype-resources/src/main/java/CrawlTopology.java
@@ -1,14 +1,12 @@
-package ${package};
-
-/**
- * Licensed to DigitalPebble Ltd under one or more
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * DigitalPebble licenses this file to You under the Apache License, Version 2.0
+ * The ASF licenses this file to you under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,6 +14,7 @@ package ${package};
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package ${package};
 
 import org.apache.stormcrawler.*;
 import org.apache.stormcrawler.bolt.FetcherBolt;

4519d593914342b3d113e88fd9ba2ef256eda945
Richard Zowalla <rzo1@apache.org>
2024-05-17 15:06:24 +0200

    Fix formatting

--- external/playwright/src/test/java/org/apache/stormcrawler/protocol/playwright/ProtocolTest.java
+++ external/playwright/src/test/java/org/apache/stormcrawler/protocol/playwright/ProtocolTest.java
@@ -31,8 +31,6 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Tests for Playwright protocol implementation. Chrome should be running on localhost, whether is

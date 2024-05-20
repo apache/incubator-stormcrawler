abd9918d30aad3723c970fe88ce1adaa0d2fdc03
Markos Volikas <115570991+mvolikas@users.noreply.github.com>
2024-05-20 16:18:59 +0300

    Fix #1230: Set sitemap key before outlink processing (#1231)
    Co-authored-by: Markos Volikas <markos.volikas@contexity.ch>

--- core/src/main/java/org/apache/stormcrawler/bolt/SiteMapParserBolt.java
+++ core/src/main/java/org/apache/stormcrawler/bolt/SiteMapParserBolt.java
@@ -128,6 +128,10 @@ public class SiteMapParserBolt extends StatusEmitterBolt {
             return;
         }
 
+        // mark the current doc as a sitemap
+        // as it won't have the k/v if it is a redirected sitemap
+        metadata.setValue(isSitemapKey, "true");
+
         List<Outlink> outlinks;
         try {
             outlinks = parseSiteMap(url, content, ct, metadata);
@@ -144,10 +148,6 @@ public class SiteMapParserBolt extends StatusEmitterBolt {
             collector.ack(tuple);
             return;
         }
-
-        // mark the current doc as a sitemap
-        // as it won't have the k/v if it is a redirected sitemap
-        metadata.setValue(isSitemapKey, "true");
 
         // apply the parse filters if any to the current document
         ParseResult parse = new ParseResult(outlinks);

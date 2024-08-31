7c6b32063536f3a1d5ed3d059059023baa915d49
Markos Volikas <115570991+mvolikas@users.noreply.github.com>
2024-08-31 16:14:56 +0300

    #851 Merge branch 851 into main (#1256)
    * Initial code for using Docker image for testing SOLR - WIP
    
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>
    
    * Merge from main
    
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>
    
    * Add (#851): Solr IndexerBolt basic test / Refactor Solr tests (#1240)
    
    * Add (#851): Solr IndexerBolt basic test / Refactor Solr tests
    
    * #851 add solr docs check / refactor
    
    * #851 remove left pom dependency
    
    * #851 core.properties headers
    
    * #851 update license headers
    
    ---------
    
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>
    Co-authored-by: Julien Nioche <julien@digitalpebble.com>

---
+++ external/solr/src/test/java/org/apache/stormcrawler/solr/persistence/IndexerBoltTest.java
@@ -0,0 +1,125 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.stormcrawler.solr.persistence;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.Http2SolrClient;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.tuple.Tuple;
+import org.apache.stormcrawler.Metadata;
+import org.apache.stormcrawler.TestOutputCollector;
+import org.apache.stormcrawler.TestUtil;
+import org.apache.stormcrawler.indexing.AbstractIndexerBolt;
+import org.apache.stormcrawler.solr.bolt.IndexerBolt;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IndexerBoltTest extends SolrContainerTest {
+    private IndexerBolt bolt;
+    protected TestOutputCollector output;
+
+    private static final Logger LOG = LoggerFactory.getLogger(IndexerBoltTest.class);
+
+    @Before
+    public void setupIndexerBolt() throws IOException, InterruptedException {
+        container.start();
+        createCore("docs");
+
+        bolt = new IndexerBolt();
+        output = new TestOutputCollector();
+
+        Map<String, Object> conf = new HashMap<>();
+        conf.put(AbstractIndexerBolt.urlFieldParamName, "url");
+        conf.put(AbstractIndexerBolt.textFieldParamName, "content");
+        conf.put(AbstractIndexerBolt.canonicalMetadataParamName, "canonical");
+        conf.put("solr.indexer.url", getSolrBaseUrl() + "/docs");
+
+        bolt.prepare(conf, TestUtil.getMockedTopologyContext(), new OutputCollector(output));
+    }
+
+    @After
+    public void close() {
+        LOG.info("Closing indexer bolt and Solr container");
+        bolt.cleanup();
+        container.close();
+        output = null;
+    }
+
+    private Future<Integer> index(String url, String text, Metadata metadata) {
+        Tuple tuple = mock(Tuple.class);
+        when(tuple.getStringByField("text")).thenReturn(text);
+        when(tuple.getStringByField("url")).thenReturn(url);
+        when(tuple.getValueByField("metadata")).thenReturn(metadata);
+        bolt.execute(tuple);
+
+        return executorService.submit(
+                () -> {
+                    var outputSize = output.getAckedTuples().size();
+                    while (outputSize == 0) {
+                        Thread.sleep(100);
+                        outputSize = output.getAckedTuples().size();
+                    }
+                    return outputSize;
+                });
+    }
+
+    @Test
+    public void basicTest()
+            throws SolrServerException,
+                    IOException,
+                    ExecutionException,
+                    InterruptedException,
+                    TimeoutException {
+
+        String url = "https://www.url.net/something";
+
+        Metadata md = new Metadata();
+
+        md.addValue("someKey", "someValue");
+
+        index(url, "test", md).get(10, TimeUnit.SECONDS);
+
+        assertEquals(1, output.getAckedTuples().size());
+
+        // Make sure the document is indexed in Solr
+        SolrClient client = new Http2SolrClient.Builder(getSolrBaseUrl() + "/docs").build();
+        client.commit();
+
+        SolrQuery query = new SolrQuery("*:*");
+        SolrDocumentList solrDocs = client.query(query).getResults();
+
+        assertEquals(1, solrDocs.getNumFound());
+        assertEquals("[test]", solrDocs.get(0).get("content").toString());
+    }
+}
---
+++ external/solr/src/test/java/org/apache/stormcrawler/solr/persistence/StatusBoltTest.java
@@ -0,0 +1,117 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.stormcrawler.solr.persistence;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.tuple.Tuple;
+import org.apache.stormcrawler.Metadata;
+import org.apache.stormcrawler.TestOutputCollector;
+import org.apache.stormcrawler.TestUtil;
+import org.apache.stormcrawler.persistence.Status;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StatusBoltTest extends SolrContainerTest {
+    private StatusUpdaterBolt bolt;
+    protected TestOutputCollector output;
+
+    private static final Logger LOG = LoggerFactory.getLogger(StatusBoltTest.class);
+
+    @Before
+    public void setupStatusBolt() throws IOException, InterruptedException {
+        container.start();
+        createCore("status");
+
+        bolt = new StatusUpdaterBolt();
+        output = new TestOutputCollector();
+
+        Map<String, Object> conf = new HashMap<>();
+        conf.put("scheduler.class", "org.apache.stormcrawler.persistence.DefaultScheduler");
+        conf.put("status.updater.cache.spec", "maximumSize=10000,expireAfterAccess=1h");
+        conf.put("solr.status.url", getSolrBaseUrl() + "/status");
+
+        bolt.prepare(conf, TestUtil.getMockedTopologyContext(), new OutputCollector(output));
+    }
+
+    @After
+    public void close() {
+        LOG.info("Closing updater bolt and SOLR container");
+        bolt.cleanup();
+        container.close();
+        output = null;
+    }
+
+    private Future<Integer> store(String url, Status status, Metadata metadata) {
+        Tuple tuple = mock(Tuple.class);
+        when(tuple.getValueByField("status")).thenReturn(status);
+        when(tuple.getStringByField("url")).thenReturn(url);
+        when(tuple.getValueByField("metadata")).thenReturn(metadata);
+        bolt.execute(tuple);
+
+        return executorService.submit(
+                () -> {
+                    var outputSize = output.getAckedTuples().size();
+                    while (outputSize == 0) {
+                        Thread.sleep(100);
+                        outputSize = output.getAckedTuples().size();
+                    }
+                    return outputSize;
+                });
+    }
+
+    @Test
+    public void basicTest() throws ExecutionException, InterruptedException, TimeoutException {
+
+        String url = "https://www.url.net/something";
+
+        Metadata md = new Metadata();
+
+        md.addValue("someKey", "someValue");
+
+        store(url, Status.DISCOVERED, md).get(10, TimeUnit.SECONDS);
+
+        assertEquals(1, output.getAckedTuples().size());
+    }
+
+    @Test
+    public void errorTest() throws Exception {
+
+        String url = "https://www.url.net/something";
+
+        Metadata md = new Metadata();
+
+        md.addValue("someKey", "someValue");
+
+        store(url, Status.ERROR, md).get(10, TimeUnit.SECONDS);
+
+        assertEquals(1, output.getAckedTuples().size());
+    }
+}
